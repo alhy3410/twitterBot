b1444687b9a69060e08c62e2449419a2053649d1
Andrew <student@epicodus.com>
2015-06-25 14:43:34 -0700

    Update with languages

--- twitterBot.rb
+++ twitterBot.rb
@@ -11,6 +11,9 @@ end
 print "Type the search term you are looking for: "
 searchTerm = $stdin.gets.chomp
 
+print "What language would you like to see: "
+lang = $stdin.gets.chomp
+
 print "Type the name of the file you want to record to: "
 filename = $stdin.gets.chomp
 
@@ -19,7 +22,7 @@ targetFileToWrite.truncate(0)
 #client.update("Second Post to test!") #This will post the tweet with the string
 
 tweetHold = []
-client.search(searchTerm).take(1000).each do |tweet|
+client.search(searchTerm, lang: lang).take(1000).each do |tweet|
   # Push all the tweets collected one by one to the default output (command-line, usually)
 
   tweetHold.push(tweet.text)
